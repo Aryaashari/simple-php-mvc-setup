332eb2dbfef9a1a882c441cf818bacd47c616809
Arya <aryaashari100@gmail.com>
2022-08-31 12:36:25 +0700

    change some field on domain wallet and history

--- app/domain/Wallet.php
+++ app/domain/Wallet.php
@@ -4,10 +4,11 @@ namespace Ewallet\Domain;
 
 class Wallet {
 
-    public ?int $id, $user_id, $balance, $pin;
+    public ?int $id, $user_id, $pin;
+    public ?float  $balance;
     public ?string $create_time, $update_time;
 
-    public function __construct(?int $id = null, ?int $user_id = null, ?int $balance = null, ?int $pin = null, ?string $create_time = null, ?string $update_time = null)
+    public function __construct(?int $id = null, ?int $user_id = null, ?float $balance = null, ?int $pin = null, ?string $create_time = null, ?string $update_time = null)
     {
         $this->id = $id;
         $this->user_id = $user_id;
