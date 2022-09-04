211b85736d7d3ecbce2f896205d4efc3a06cb18c
Arya <aryaashari100@gmail.com>
2022-09-04 15:59:09 +0700

    create file authservice

---
+++ app/service/AuthService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ewallet\Service;
+
+use Ewallet\Model\Auth\RegisterRequest;
+
+class AuthService {
+
+
+    public function register(RegisterRequest $register) {
+
+        // Validasi request
+
+        // Create Data User
+
+        // Send verification link to user's email
+
+        // Return Response
+
+    }
+
+
+}
