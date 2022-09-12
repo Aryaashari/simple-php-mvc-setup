0537ea2a4e888984a2767c64b898ee4214aa1b8a
Arya <aryaashari100@gmail.com>
2022-09-12 14:09:50 +0700

    create function delete, find by id, update, in session repo

--- app/repository/SessionRepository.php
+++ app/repository/SessionRepository.php
@@ -15,16 +15,50 @@ class SessionRepository {
     }
 
 
+    public function findById(int $id) : ?Session {
+        try {
+            $stmt = $this->db->prepare("SELECT id,user_id, ip_address, user_agent, last_activated_time, expire_time, create_time FROM sessions WHERE id=?");
+            $stmt->execute([$id]);
+            if ($session = $stmt->fetch()) {
+                return new Session($session["id"], $session["user_id"], $session["ip_address"], $session["user_agent"], $session["last_activated_time"], $session["expire_time"], $session["create_time"]);
+            }
+            return null;
+        } catch(\Exception $e) {
+            throw $e;
+        }
+    }
+
     public function create(int $userId, string $ipAddress, string $userAgent) : Session {
         try {
             $dateNow = date("Y-m-d H:i:s", time());
             $expire = date("Y-m-d H:i:s", time()+300);
-            $id = (int)microtime();
+            $id = rand(100,9999) + time();
             $stmt = $this->db->prepare("INSERT INTO sessions(id,user_id, ip_address, user_agent, last_activated_time, expire_time, create_time) VALUES(?,?,?,?,?,?,?)");
             $stmt->execute([$id,$userId, $ipAddress, $userAgent, $dateNow,$expire,$dateNow]);
 
             return new Session($id, $userId, $ipAddress, $userAgent, $dateNow, $expire, $dateNow);
         } catch(\Exception $e) {
+            throw $e;
+        }
+    }
+
+
+    public function update(Session $session) : Session {
+        try {
+            $stmt = $this->db->prepare("UPDATE sessions SET last_activated_time = ?, expire_time = ? WHERE id=?");
+            $stmt->execute([$session->last_activated_time, $session->expire_time, $session->id]);
+            return $session;
+        } catch(\Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function delete(int $sessionId) : void {
+        try {
+            $stmt = $this->db->prepare("DELETE FROM sessions WHERE id=?");
+            $stmt->execute([$sessionId]);
+            return;
+        } catch (\Exception $e) {
             throw $e;
         }
     }
--- test/Repository/SessionRepoTest.php
+++ test/Repository/SessionRepoTest.php
@@ -20,15 +20,60 @@ class SessionRepoTest extends TestCase {
 
     public function testCreate() {
         $user = $this->userRepo->create(new User(null, "Arya", "aryaashari100@gmail.com", "arya", "12345678", null,true,date("Y-m-d H:i:s", time())));
-        var_dump($user);
-        // // $ipAddress = $_SERVER["REMOTE_ADDR"];
-        // var_dump($ipAddress);
-        $id = time();
-        var_dump($id);
-        // $session = $this->sessionRepo->create($user->id, "192.168.0.1", "chrome::arya-ashari");
-        // var_dump($session);
+
+        $session = $this->sessionRepo->create($user->id, "192.168.0.1", "chrome::arya-ashari");
+        var_dump($session);
         $this->assertIsObject($user);
-        // $this->assertIsObject($session);
+        $this->assertIsObject($session);
+    }
+
+    public function testFindById() {
+        $user = $this->userRepo->create(new User(null, "Arya", "aryaashari100@gmail.com", "arya", "12345678", null,true,date("Y-m-d H:i:s", time())));
+
+        $session1 = $this->sessionRepo->create($user->id, "192.168.0.1", "chrome::arya-ashari");
+
+        $session = $this->sessionRepo->findById($session1->id);
+
+        $this->assertSame($session1->id, $session->id);
+        $this->assertSame($session1->user_id, $session->user_id);
+        $this->assertSame($session1->ip_address, $session->ip_address);
+        $this->assertSame($session1->user_agent, $session->user_agent);
+        $this->assertSame($session1->last_activated_time, $session->last_activated_time);
+        $this->assertSame($session1->expire_time, $session->expire_time);
+        $this->assertSame($session1->create_time, $session->create_time);
+    }
+
+    public function testUpdate() { 
+        $user = $this->userRepo->create(new User(null, "Arya", "aryaashari100@gmail.com", "arya", "12345678", null,true,date("Y-m-d H:i:s", time())));
+
+        $session1 = $this->sessionRepo->create($user->id, "192.168.0.1", "chrome::arya-ashari");
+
+        $session1->user_agent = "chrome::dul-fitrah";
+        $session1->last_activated_time = time();
+        $session1->expire_time = time() + 300;
+        $session2 = $this->sessionRepo->update($session1);
+
+        var_dump($session2);
+
+        $this->assertSame($session1->id, $session2->id);
+        $this->assertSame($session1->user_id, $session2->user_id);
+        $this->assertSame($session1->ip_address, $session2->ip_address);
+        $this->assertSame($session1->user_agent, $session2->user_agent);
+        $this->assertSame($session1->last_activated_time, $session2->last_activated_time);
+        $this->assertSame($session1->expire_time, $session2->expire_time);
+        $this->assertSame($session1->create_time, $session2->create_time);
+    }
+
+    public function testDelete() : void {
+        $user = $this->userRepo->create(new User(null, "Arya", "aryaashari100@gmail.com", "arya", "12345678", null,true,date("Y-m-d H:i:s", time())));
+
+        $session1 = $this->sessionRepo->create($user->id, "192.168.0.1", "chrome::arya-ashari");
+
+        $this->sessionRepo->delete($session1->id);
+
+        $session = $this->sessionRepo->findById($session1->id);
+
+        $this->assertNull($session);
     }
 
 }
