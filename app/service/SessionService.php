8c521e8e66392ea684719b84c35cf05584b09d13
Arya <aryaashari100@gmail.com>
2022-09-12 15:49:06 +0700

    create function create session, create model for session create response

--- app/service/SessionService.php
+++ app/service/SessionService.php
@@ -2,6 +2,9 @@
 
 namespace Ewallet\Service;
 
+use Ewallet\Domain\Session;
+use Ewallet\Model\Session\CreateSessionRequest;
+use Ewallet\Model\Session\CreateSessionResponse;
 use Ewallet\Repository\SessionRepository;
 use Ewallet\Repository\UserRepository;
 
@@ -14,6 +17,16 @@ class SessionService {
     {
         $this->sessionRepo = $sessionRepo;
         $this->userRepo = $userRepo;
+    }
+    
+
+    public function createSession(CreateSessionRequest $request) : CreateSessionResponse {
+        try {
+            $session = $this->sessionRepo->create($request->userId, $request->ipAddress, $request->userAgent);
+            return new CreateSessionResponse($session->id);
+        } catch(\Exception $e) {
+            throw $e;
+        }
     }
 
 }
